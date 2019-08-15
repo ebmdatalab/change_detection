5f453f868dd600cec7b82b3e8ff8dbabae0eb147
alexwalker <alexwalkercebm@gmail.com>
2019-08-15 11:09:24 +0100

    Delete install_packages.R

--- install_packages.R
+++
@@ -1,6 +0,0 @@
-
-##################Install necessary packages#####################
-
-install.packages("zoo", repos = "http://cran.us.r-project.org")
-install.packages("caTools", repos = "http://cran.us.r-project.org")
-install.packages("gets", repos = "https://CRAN.R-project.org")
